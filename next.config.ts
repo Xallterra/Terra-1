aa5fe6148baa528a6cb3c68be266c95413826070
xallterra <sandeshsunuwar18@gmial.com>
2026-06-05 22:55:39 -0500

    Redirect apex domain to www

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,17 @@ const nextConfig: NextConfig = {
   output: 'standalone',
   experimental: {
     typedRoutes: true
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'makriva.com' }],
+        destination: 'https://www.makriva.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
